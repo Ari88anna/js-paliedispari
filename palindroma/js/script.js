5e0fcf3e32adb670676f0014521028cfc300aee4
Arianna Carotti <arianna.carotti@gmail.com>
2021-03-25 15:38:45 +0100

    cominciato

---
+++ palindroma/js/script.js
@@ -0,0 +1,28 @@
+// Chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+// chiedo all'utente di inserire una parola (stringa)
+var parolaUtente = prompt('Inserisci una parola');
+
+// suddivido i caratteri della parola
+
+
+
+// creo una funzione per capire se la parola inserita è palindroma
+
+function verifica(parolaDaValutare) {
+    
+}
+for ( var i= 0; i < parolaUtente.length; i++) {
+    
+    var lettereParola = parolaUtente[i];
+    // console.log(lettereParola); 
+
+    var lettereInvertite = parolaUtente[parolaUtente.length -1 -i];
+    console.log(lettereInvertite)
+
+    if() {
+        
+    }
+}
